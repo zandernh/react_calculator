012ce852d479791144e6d3e2b27ea5485749035d
unknown <zandernh@gmail.com>
2025-03-31 08:20:28 +0200

    Uploading finished calculator project files

---
+++ src/DigitButton.jsx
@@ -0,0 +1,7 @@
+import { ACTIONS } from "./App"
+
+export function DigitButton( { dispatch, digit } ) {
+
+    return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}> { digit } </button>
+
+}
---
+++ src/OperationButton.jsx
@@ -0,0 +1,7 @@
+import { ACTIONS } from "./App"
+
+export function OperationButton( { dispatch, operation } ) {
+
+    return <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}> { operation } </button>
+
+}
